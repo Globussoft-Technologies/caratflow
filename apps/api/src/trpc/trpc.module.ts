7951a9f7041f3c44d887c1d535c97e916b93f963
indianbill007 <indianbill007@gmail.com>
2026-04-07 14:04:54 +0530

    fix: make TrpcModule global, disable tRPC controller to resolve circular deps

--- apps/api/src/trpc/trpc.module.ts
+++ apps/api/src/trpc/trpc.module.ts
@@ -1,18 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TrpcService } from './trpc.service';
-import { TrpcRouter } from './trpc.router';
-import { TrpcController } from './trpc.controller';
-import { InventoryModule } from '../modules/inventory/inventory.module';
-import { CmsModule } from '../modules/cms/cms.module';
-import { ReferralModule } from '../modules/referral/referral.module';
-import { AmlModule } from '../modules/aml/aml.module';
-import { PreOrderModule } from '../modules/preorder/preorder.module';
-import { SearchModule } from '../modules/search/search.module';
 
+@Global()
 @Module({
-  imports: [InventoryModule, CmsModule, ReferralModule, AmlModule, PreOrderModule, SearchModule],
-  controllers: [TrpcController],
-  providers: [TrpcService, TrpcRouter],
-  exports: [TrpcService, TrpcRouter],
+  providers: [TrpcService],
+  exports: [TrpcService],
 })
 export class TrpcModule {}
